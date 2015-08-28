d3d7396a10bc73283daa21ce03c4156fa11767a3
ramsey <join@com1.kr>
2015-08-28 14:16:39 +0900

    두번째 과제 제출

---
+++ JavaPractice/src/org/dimigo/inheritance/SmartPhone.java
@@ -0,0 +1,44 @@
+package org.dimigo.inheritance;
+
+public class SmartPhone {
+	
+	private String model;
+	private String company;
+	private int price;
+	
+	public SmartPhone() {
+		
+	}
+	
+	public SmartPhone(String model, String company, int price) {
+		this.model   = model;
+		this.company = company;
+		this.price   = price;
+	}
+	
+	public void turnOn() {
+		System.out.println(this.model + "의 전원을 켭니다.");
+	}
+	
+	public void turnOff() {
+		System.out.println(this.model + "의 전원을 끕니다.");
+	}
+	
+	public void pay() {
+		System.out.println(this.model + "결제합니다.");
+	}
+	
+	public void useSpecialFunction() {
+		if(this instanceof IPhone) {
+			((IPhone) this).useAirDrop();
+		}
+		else if(this instanceof Galaxy) {
+			((Galaxy) this).useWirelessCharging();
+		}
+	}
+	
+	public String toString() {
+		return "모델명 : " + this.model + ", 제조사 : " + this.company + ", 가격 : " + String.format("%,d",this.price) + "원";
+	}
+	
+}
