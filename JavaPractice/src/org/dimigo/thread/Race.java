9755056903f2c81e627c53646ebd3c41f169ceec
ramsey <join@com1.kr>
2015-11-06 14:24:35 +0900

    9번째 실습과제 수정
    main thread start, end 추가

--- JavaPractice/src/org/dimigo/thread/Race.java
+++ JavaPractice/src/org/dimigo/thread/Race.java
@@ -4,6 +4,8 @@ public class Race {
 
 	public static void main(String[] args) {
 		
+		System.out.println("main thread start");
+		
 		Thread t1 = new Runner("홍길동");
 		Thread t2 = new Runner("홍길순");
 		
@@ -12,6 +14,8 @@ public class Race {
 		
 		t1.start();
 		t2.start();
+		
+		System.out.println("main thread end");
 		
 	}
 
