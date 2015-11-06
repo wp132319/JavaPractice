895ce6870c55e1db603cbb0a95a03990327348eb
ramsey <join@com1.kr>
2015-11-06 14:05:28 +0900

    9번째 실습과제 제출 수정
    우선순위 지정

--- JavaPractice/src/org/dimigo/thread/Race.java
+++ JavaPractice/src/org/dimigo/thread/Race.java
@@ -7,6 +7,9 @@ public class Race {
 		Thread t1 = new Runner("홍길동");
 		Thread t2 = new Runner("홍길순");
 		
+		t1.setPriority(Thread.MAX_PRIORITY);
+		t1.setPriority(Thread.MIN_PRIORITY);
+		
 		t1.start();
 		t2.start();
 		
